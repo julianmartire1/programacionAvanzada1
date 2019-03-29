98b699e7151ba7befe1f517891b9d39dde01b16f
julianmartire1 <julianmartire.jm@gmail.com>
2019-03-28 22:07:42 -0300

    clase 2

---
+++ Clase2/src/Ejercicios/Ejercicio5.java
@@ -0,0 +1,7 @@
+package Ejercicios;
+
+public class Ejercicio5  {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Clase2/src/Ejercicios/ICalcular.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public interface ICalcular {
+	double sumar(Number N1,Number N2);
+
+	double restar(Number N1,Number N2);
+
+	double multiplicar (Number N1,Number N2);
+
+	double dividir (Number N1,Number N2);
+}
